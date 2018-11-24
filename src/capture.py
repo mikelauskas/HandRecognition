40fba96e9f99186cbfc4535b02b6e121efa16906
FlorianPeron <peron.florian01@gmail.com>
2018-11-24 14:52:44 +0100

    new main

--- src/capture.py
+++ src/capture.py
@@ -3,11 +3,7 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
-        img_counter = int(sys.argv[1])
-        _ = Image.captureImageBW(img_counter)
-    elif len(sys.argv) == 3:
-        img_counter = int(sys.argv[1])
-        path = sys.argv[2]
-        _ = Image.captureImageBW(img_counter, path)
+        path = sys.argv[1]
+        _ = Image.captureImageBW(path, width=200)
     else:
-        _ = Image.captureImageBW()
+        _ = Image.captureImageBW(width=200)
